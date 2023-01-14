ac5df86e4a85c0d0d571e321159940a7e182498d
Dosev <119459914+Dosev91@users.noreply.github.com>
2023-01-15 00:19:54 +0200

    Add files via upload

---
+++ Print and Sum.js
@@ -0,0 +1,14 @@
+function solve(start, end) {
+
+    let sum = 0;
+    let numbers = '';
+
+    for (let i = start; i <= end; i++) {
+        numbers += i + ' ';
+        sum += i;
+    }
+    console.log(numbers);
+    console.log(`Sum: ${sum}`);
+
+}
+solve(5, 10)

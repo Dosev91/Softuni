760062090552e940dfe2a3368caa7c831e4453ab
Dosev <119459914+Dosev91@users.noreply.github.com>
2023-02-12 22:44:06 +0200

    Add files via upload

---
+++ Add and subtract 322.js
@@ -0,0 +1,12 @@
+function subtract(num1, num2, num3) {
+
+    let result = sum(num1, num2) - num3;
+    console.log(result);
+}
+
+function sum(x, y) {
+
+    return x + y;
+}
+
+subtract(42, 58, 100)

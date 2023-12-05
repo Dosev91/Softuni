f66a41a70bcd27bcc2b9d81453f77b68e1908131
Hristian Dosev <hristiqnpirona@gmail.com>
2023-12-06 00:54:25 +0200

    solved last task before the exam

--- JS Advanced/Exam prep/28.Gifts Christmas/01. Christmas Gifts Delivery Skeleton/app.js
+++ JS Advanced/Exam prep/28.Gifts Christmas/01. Christmas Gifts Delivery Skeleton/app.js
@@ -1,3 +1,50 @@
 function solution() {
-    //TO DO
+    const listSection = document.getElementsByClassName('card');
+    const ulList = listSection[1].querySelector('ul');
+    const sendGift = listSection[2].querySelector('ul');
+    const dscardGift = listSection[3].querySelector('ul');
+    const input = listSection[0].querySelector('input');
+
+    const addBtn1 = listSection[0].querySelector('button');
+
+    addBtn1.addEventListener("click", () => {
+        const newLi = document.createElement("li");
+        newLi.className = "gift";
+        newLi.textContent = input.value;
+
+        const sendBtn = document.createElement("button");
+        sendBtn.textContent = "Send";
+        sendBtn.className = "sendButton";
+
+        const discardBtn = document.createElement("button");
+        discardBtn.textContent = "Discard";
+        discardBtn.className = "discardButton";
+
+        sendBtn.addEventListener("click", () => {
+            const li = document.createElement("li");
+            li.className = "gift";
+            newLi.removeChild(sendBtn);
+            newLi.removeChild(discardBtn);
+            li.textContent = newLi.textContent;
+
+            sendGift.appendChild(li);
+            ulList.removeChild(newLi);
+        });
+        discardBtn.addEventListener("click", () => {
+            const li = document.createElement("li");
+            li.className = "gift";
+            newLi.removeChild(sendBtn);
+            newLi.removeChild(discardBtn);
+            li.textContent = newLi.textContent;
+
+            dscardGift.appendChild(li);
+            ulList.removeChild(newLi);
+        });
+
+        newLi.appendChild(sendBtn);
+        newLi.appendChild(discardBtn);
+        ulList.appendChild(newLi);
+
+        input.value = "";
+    });
 }

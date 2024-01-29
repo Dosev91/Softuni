0fcf9d9f01a440136ffcee5986f8a0df3f6a6664
Hristian Dosev <hristiqnpirona@gmail.com>
2024-01-30 00:32:17 +0200

    solved task

--- JS Applications/02.Asynchronos-Exercise/01.Bus-Stop/app.js
+++ JS Applications/02.Asynchronos-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const stopIdInput = document.getElementById('stopId').value;
+  const buses = document.getElementById('buses');
+  const stopName = document.getElementById('stopName');
+
+  buses.innerHTML = '';
+  stopName.textContent = `Loading`;
+
+  try {
+    const response = await fetch(
+      `http://localhost:3030/jsonstore/bus/businfo/${stopIdInput}`,
+    );
+
+    const data = await response.json();
+    stopName.textContent = data.name;
+
+    Object.entries(data.buses).forEach(([busId, time]) => {
+      const li = document.createElement('li');
+      li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+      buses.appendChild(li);
+    });
+  } catch (err) {
+    stopName.textContent = `Error`;
+  }
 }

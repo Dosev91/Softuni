a79cf1cf5ac992c51a1fc50588d1e74051e84dce
Hristian Dosev <hristiqnpirona@gmail.com>
2024-02-02 01:52:08 +0200

    solved task

--- JS Applications/02.Asynchronos-Exercise/02.Bus-Schedule/app.js
+++ JS Applications/02.Asynchronos-Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,39 @@
 function solve() {
+  const deparInput = document.getElementById('depart');
+  const arriveInput = document.getElementById('arrive');
+  const info = document.getElementById('info');
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  let nextStop = 'depot';
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  async function depart() {
+    arriveInput.disabled = false;
+    deparInput.disabled = true;
 
-    return {
-        depart,
-        arrive
-    };
+    const urlStop = await fetch(
+      ` http://localhost:3030/jsonstore/bus/schedule/${nextStop}`,
+    );
+    const stop = await urlStop.json();
+
+    info.textContent = `Next stop ${stop.name}`;
+  }
+
+  async function arrive() {
+    deparInput.disabled = false;
+    arriveInput.disabled = true;
+
+    const urlStop = await fetch(
+      ` http://localhost:3030/jsonstore/bus/schedule/${nextStop}`,
+    );
+    const stop = await urlStop.json();
+
+    info.textContent = `Arriving at ${stop.name}`;
+    nextStop = stop.next;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
